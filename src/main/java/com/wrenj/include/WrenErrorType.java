0b9403058ab14102014d6be240fd4e19200ae787
gaetgu <gabriel.ethan@gmail.com>
2022-06-01 14:11:09 -0600

    Added the WrenInterpretResult and WrenErrorType enums

---
+++ src/main/java/com/wrenj/include/WrenErrorType.java
@@ -0,0 +1,12 @@
+package com.wrenj.include;
+
+public enum WrenErrorType {
+    // A syntax or resolution error detected at compile time
+    WREN_ERROR_COMPILE,
+
+    // The error message for a runtime error
+    WREN_ERROR_RUNTIME,
+
+    // One entry of a runtime error's stack trace
+    WREN_ERROR_STACK_TRACE
+}
---
+++ src/main/java/com/wrenj/include/WrenInterpretResult.java
@@ -0,0 +1,7 @@
+package com.wrenj.include;
+
+public enum WrenInterpretResult {
+    WREN_RESULT_SUCCESS,
+    WREN_RESULT_COMPILE_ERROR,
+    WREN_RESULT_RUNTIME_ERROR
+}
